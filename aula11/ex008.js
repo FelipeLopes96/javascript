971a9ea44d69a78b5574b51f81af01e487154635
Felipe Lopes <felipe.clopes@hotmail.com>
2022-08-09 12:59:30 -0300

    atualizaçao

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 125
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60 && vel < 120) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log('Dirija sempre usando o cinto de segurança')
+
